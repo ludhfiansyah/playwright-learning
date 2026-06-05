47648b65200cbf8eba0312af5841cf16fe097ec5
ludhfi <ludhfi@gmail.com>
2026-06-05 18:03:34 +0700

    finish section 7.44 video

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,44 +1,38 @@
 const { test, expect } = require('@playwright/test');
-const { copyFileSync } = require('node:fs');
 
 test.only('Assignment Test 1 - Client App', async ({ page }) => {
   await page.goto('https://rahulshettyacademy.com/client/#/auth/login');
   console.log(await page.title());
 
-  const nameOfUser = page.getByPlaceholder('email@example.com');
-  const passwordOfUser = page.getByPlaceholder('enter your password');
+  const email = page.getByPlaceholder('email@example.com');
+  const password = page.getByPlaceholder('enter your passsword');
   const logInClick = page.getByRole('button', { name: 'Login' });
   const cardTitle = page.locator('.card-body b');
 
   const productName = 'ZARA COAT 3';
   const product = page.locator('.card-body');
 
-  await page.waitForLoadState('networkidle');
-  await nameOfUser.fill('pipiiww@gmail.com');
-  await passwordOfUser.fill('P@ssw0rd');
+  await email.fill('pipiiww@gmail.com');
+  await password.fill('P@ssw0rd');
   await logInClick.click();
 
   // await page.waitForLoadState('networkidle');
   await cardTitle.first().waitFor();
-  const allTitles = await cardTitle.allTextContents();
-  console.log(allTitles);
-  // ZARA COAT 3
-  const count = await product.count();
-  console.log(count);
-  for (let i = 0; i < count; i++) {
-    if ((await product.nth(i).locator('b').textContent()) === productName) {
-      // write the logic to add product to cart
-      await product.nth(i).locator('text= Add To Cart').click();
-      break;
-    }
-  }
+  await page
+    .locator('.card-body')
+    .filter({ hasText: 'ZARA COAT 3' })
+    .getByRole('button', { name: 'Add to Cart' })
+    .click();
 
-  await page.locator('[routerlink*="cart"]').click();
+  await page
+    .getByRole('listitem')
+    .getByRole('button', { name: 'Cart' })
+    .click();
+
   await page.locator('div li').first().waitFor();
-  const bool = await page.locator('h3:has-text("Zara Coat 3")').isVisible();
-  expect(bool).toBeTruthy();
+  await expect(page.getByText('ZARA COAT 3')).toBeVisible();
+  await page.getByRole('button', { name: 'Checkout' }).click();
 
-  await page.locator('button[type="button"]').last().click();
   const textCCNumber = await page
     .locator('.text-validated')
     .first()
@@ -48,34 +42,20 @@ test.only('Assignment Test 1 - Client App', async ({ page }) => {
   await page.locator('input[class="input txt"]').first().fill('342');
   await page.locator('input[class="input txt"]').last().fill('Burhanuddin');
 
-  await page
-    .locator('[placeholder*="Country"]')
-    .pressSequentially('ind', { delay: 100 });
-  const dropdown = page.locator('.ta-results');
-  await dropdown.waitFor();
-  const optionCount = await dropdown.locator('button').count();
-  console.log(optionCount);
-  for (let i = 0; i < optionCount; i++) {
-    const text = await dropdown.locator('button').nth(i).textContent();
-    if (text === ' Indonesia') {
-      await dropdown.locator('button').nth(i).click();
-      break;
-    }
-  }
+  await page.getByPlaceholder('Select Country').pressSequentially('indo');
 
-  expect(page.locator('.user__name [type="text"]').first()).toHaveText(
-    'pipiiww@gmail.com',
-  );
-  await page.locator('.action__submit').click();
-  await expect(page.locator('.hero-primary')).toHaveText(
-    ' Thankyou for the order. ',
-  );
+  await page.getByRole('button', { name: 'Indonesia' }).click();
+  await page.getByText('PLACE ORDER').click();
+
+  await expect(page.getByText('Thankyou for the order.')).toBeVisible();
+
   const orderId = await page
     .locator('.em-spacer-1 .ng-star-inserted')
     .textContent();
   console.log(orderId);
 
-  await page.locator('ul li [routerlink$="/dashboard/myorders"]').click();
+  // await page.locator('ul li [routerlink$="/dashboard/myorders"]').click();
+  await page.getByRole('button', { name: 'ORDERS' }).click();
 
   await page.locator('tbody').waitFor();
   const rows = await page.locator('tbody tr');
